cd84c5863275bbef5f4103caacb4c45144512999
kevinhenneigh <kevinhenneigh@hotmail.com>
2020-11-28 17:16:43 -0800

    Successfully implemented basic onCreate navigation screen. Use back button to return to menu at this time.

--- app/src/main/java/com/ebookfrenzy/tenthousanddicegame/RulesFragment.java
+++ app/src/main/java/com/ebookfrenzy/tenthousanddicegame/RulesFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import android.net.Uri;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,7 +20,7 @@ public class RulesFragment extends Fragment {
 
     public interface OnFragmentInteractionListener {
         //ToDo Update Argument type and name
-        void onFragmentInteractionListener(Uri uri);
+        void onFragmentInteraction(Uri uri);
     }
 
     // TODO: Rename parameter arguments, choose names that match
--- app/src/main/java/com/ebookfrenzy/tenthousanddicegame/ui/main/MainFragment.java
+++ app/src/main/java/com/ebookfrenzy/tenthousanddicegame/ui/main/MainFragment.java
@@ -13,6 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ebookfrenzy.tenthousanddicegame.R;
+import android.widget.Button;
+import androidx.navigation.Navigation;
+
+
 
 public class MainFragment extends Fragment {
 
@@ -33,7 +37,32 @@ public class MainFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-        // TODO: Use the ViewModel
-    }
 
+        Button rulesButton = getView().findViewById(R.id.rules_button);
+
+        rulesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.mainToRules);
+            }
+        });
+
+        Button gameButton = getView().findViewById(R.id.play_game_button);
+
+        gameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.mainToGame);
+            }
+        });
+
+        Button bestRoundsButton = getView().findViewById(R.id.best_rounds_button);
+
+        bestRoundsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.mainToBestRounds);
+            }
+        });
+    }
 }
